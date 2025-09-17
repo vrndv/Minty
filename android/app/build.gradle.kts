b1b8d5453daac2d5430ff5a74927aed837687f8d
Varun <varundevnic@gmail.com>
2025-09-18 00:33:45 +0530

    update compileSdk and targetSdk to 35 for improved compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,11 +10,11 @@ plugins {
 
 android {
     namespace = "com.varungg.popapp"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.varungg.popapp"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
