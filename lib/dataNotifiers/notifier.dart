babfd2366d16fc8a73bbd6db7ebdd97ae31fc0ab
Varun <varundevnic@gmail.com>
2025-10-01 23:15:35 +0530

    refactor notifier.dart

--- lib/dataNotifiers/notifier.dart
+++ lib/dataNotifiers/notifier.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 ValueNotifier<int> currentPageNotifier = ValueNotifier(0);
 ValueNotifier<String> currentUser = ValueNotifier("guest");
 ValueNotifier<String> currentEmail = ValueNotifier("guest@gmail.com");
-ValueNotifier<bool> currentTheme = ValueNotifier(false); //true = light
+ValueNotifier<String>  currentPFP= ValueNotifier("1");
 ValueNotifier<String> userID = ValueNotifier("Null");
+ValueNotifier<bool> currentTheme = ValueNotifier(false); //true = light
 ValueNotifier<bool> isSearch=ValueNotifier(false);
 ValueNotifier<bool> isProfanity=ValueNotifier(true);
 ValueNotifier<int> newVer = ValueNotifier(1);
