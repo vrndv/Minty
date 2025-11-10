494f3c1f2dc56c34c21fa6b83439c6d2f2809567
vrndv <varundevnic@gmail.com>
2025-11-10 17:01:58 +0530

    add button(ai chat )

--- lib/screen/views/pages/users.dart
+++ lib/screen/views/pages/users.dart
@@ -1,3 +1,4 @@
+import 'package:SHADE/screen/views/pages/aichatpage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:SHADE/dataNotifiers/notifier.dart';
@@ -5,6 +6,7 @@ import 'package:SHADE/screen/views/widgets/avatar.dart';
 import 'package:SHADE/services/chat_services.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
+import 'package:lottie/lottie.dart';
 
 class Users extends StatefulWidget {
   const Users({super.key});
@@ -24,7 +26,8 @@ class _UsersState extends State<Users> {
       curve: Curves.fastOutSlowIn,
     );
   }
-var isUndo = false;
+
+  var isUndo = false;
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +36,7 @@ var isUndo = false;
         children: [
           Column(
             children: [
-              SizedBox(height: 5,),
+              SizedBox(height: 5),
               SearchBar(
                 onTap: () {
                   isSearch.value = true;
@@ -43,10 +46,11 @@ var isUndo = false;
                 hintText: "Search",
                 leading: Icon(Icons.search),
               ),
-
+              SizedBox(height: 10),
               Expanded(child: _buildUserList()),
             ],
           ),
+
           Positioned(
             bottom: 12,
             right: 25,
@@ -94,6 +98,35 @@ var isUndo = false;
               ),
             ),
           ),
+          Positioned(
+            bottom: 90,
+            right: 30,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return AiChat();
+                    },
+                  ),
+                );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(60),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  child: Lottie.asset(
+          'assets/rive/orb.json',
+          width: 700,
+          height: 700,
+          fit: BoxFit.contain,
+        ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -121,152 +154,153 @@ var isUndo = false;
     );
   }
 
- Widget _buildChatItem(DocumentSnapshot doc) {
-  final rawData = doc.data();
-  if (rawData == null) return const SizedBox.shrink();
+  Widget _buildChatItem(DocumentSnapshot doc) {
+    final rawData = doc.data();
+    if (rawData == null) return const SizedBox.shrink();
 
-  Map<String, dynamic> data = rawData as Map<String, dynamic>;
+    Map<String, dynamic> data = rawData as Map<String, dynamic>;
 
-  List participants = data["participants"] ?? [];
-  Map usernames = data["usernames"] ?? {};
-  Map pfps = data["pfps"] ?? {};
-  if (participants.isEmpty ||
-      !usernames.containsKey(userID.value) ||
-      usernames.containsKey("global")) {
-    return const SizedBox.shrink();
-  }
+    List participants = data["participants"] ?? [];
+    Map usernames = data["usernames"] ?? {};
+    Map pfps = data["pfps"] ?? {};
+    if (participants.isEmpty ||
+        !usernames.containsKey(userID.value) ||
+        usernames.containsKey("global")) {
+      return const SizedBox.shrink();
+    }
 
-  String otherUid = participants.firstWhere((uid) => uid != userID.value);
-  String otherUsername = usernames[otherUid] ?? "Unknown";
-  String Otherpfp = pfps[otherUid] ?? otherUsername;
-  return Container(
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      color: currentTheme.value
-          ? const Color.fromARGB(120, 211, 211, 212)
-          : const Color.fromARGB(255, 35, 36, 35),
-    ),
-    margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-    child: Slidable(
-      key: ValueKey(doc.id),
-      endActionPane: ActionPane(
-        motion: const BehindMotion(),
-        dismissible: DismissiblePane(
-          dismissThreshold: 0.3,
-          onDismissed: () async {
-            final deletedChatData = data; // Store a copy before deleting
-            final deletedChatId = doc.id;
-            ChatServices().tempDeleteChat(roomID: deletedChatId);
-            // Show snackbar with undo option
-             ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Chat with $otherUsername deleted'),
-                action: SnackBarAction(
-                  label: 'Undo',
-                  onPressed: () async {
-                    restoreChat(roomID: deletedChatId, chatData: deletedChatData);
-                    isUndo = true;
-                  },
-                ),
-                duration: const Duration(seconds: 5),
-              ),
-            );
-            Future.delayed(Duration(seconds: 5)).then((value) async {
-              if(!isUndo){
-                await ChatServices().deleteChat(roomID: deletedChatId);
-                isUndo = false;
-              }
-              
-            },);
-          },
-        ),
-        children: [
-          SlidableAction(
-            icon: Icons.delete,
-            backgroundColor: Colors.red,
-            label: "Delete",
-            onPressed: (_) async {
-              final deletedChatData = data;
+    String otherUid = participants.firstWhere((uid) => uid != userID.value);
+    String otherUsername = usernames[otherUid] ?? "Unknown";
+    String Otherpfp = pfps[otherUid] ?? otherUsername;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: currentTheme.value
+            ? const Color.fromARGB(120, 211, 211, 212)
+            : const Color.fromARGB(255, 35, 36, 35),
+      ),
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Slidable(
+        key: ValueKey(doc.id),
+        endActionPane: ActionPane(
+          motion: const BehindMotion(),
+          dismissible: DismissiblePane(
+            dismissThreshold: 0.3,
+            onDismissed: () async {
+              final deletedChatData = data; // Store a copy before deleting
               final deletedChatId = doc.id;
-
-              await ChatServices().tempDeleteChat(roomID: deletedChatId);
-
+              ChatServices().tempDeleteChat(roomID: deletedChatId);
+              // Show snackbar with undo option
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text('Chat with $otherUsername deleted'),
                   action: SnackBarAction(
                     label: 'Undo',
                     onPressed: () async {
-                    restoreChat(roomID: deletedChatId, chatData: deletedChatData);
-                    isUndo = true;
+                      restoreChat(
+                        roomID: deletedChatId,
+                        chatData: deletedChatData,
+                      );
+                      isUndo = true;
                     },
                   ),
                   duration: const Duration(seconds: 5),
                 ),
               );
               Future.delayed(Duration(seconds: 5)).then((value) async {
-              if(!isUndo){
-                await ChatServices().deleteChat(roomID: deletedChatId);
-                isUndo = false;
-              }
-              
-            }
-            );
+                if (!isUndo) {
+                  await ChatServices().deleteChat(roomID: deletedChatId);
+                  isUndo = false;
+                }
+              });
             },
           ),
-        ],
-      ),
-      child: Material(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(20),
-        child: InkWell(
-          onTap: () {
-            Future.delayed(const Duration(milliseconds: 200), () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return userChatPage(
-                      u1: otherUid,
-                      u2: userID.value,
-                      senderUsername: currentUser.value,
-                      receiverUsername: otherUsername,
-                      pfp:Otherpfp,
-                    );
-                  },
-                ),
-              );
-            });
-          },
-          borderRadius: BorderRadius.circular(20),
-          child: ListTile(
-            leading: Avatar(seed:Otherpfp, r: 25),
-            title: Text(otherUsername),
-            subtitle: Text(
-              data["lastMsg"] != null
-              ? data["reciever"] == currentUser.value
-                ?"${data["lastMsg"]}"
-                : "You:${data["lastMsg"]}"
-              :"",
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+          children: [
+            SlidableAction(
+              icon: Icons.delete,
+              backgroundColor: Colors.red,
+              label: "Delete",
+              onPressed: (_) async {
+                final deletedChatData = data;
+                final deletedChatId = doc.id;
+
+                await ChatServices().tempDeleteChat(roomID: deletedChatId);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Chat with $otherUsername deleted'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () async {
+                        restoreChat(
+                          roomID: deletedChatId,
+                          chatData: deletedChatData,
+                        );
+                        isUndo = true;
+                      },
+                    ),
+                    duration: const Duration(seconds: 5),
+                  ),
+                );
+                Future.delayed(Duration(seconds: 5)).then((value) async {
+                  if (!isUndo) {
+                    await ChatServices().deleteChat(roomID: deletedChatId);
+                    isUndo = false;
+                  }
+                });
+              },
             ),
-            trailing: Text(
-              data["lastUpdated"] != null
-                  ? _formatTimestamp(data["lastUpdated"])
-                  : "",
-              style: const TextStyle(fontSize: 12),
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+          child: InkWell(
+            onTap: () {
+              Future.delayed(const Duration(milliseconds: 200), () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return userChatPage(
+                        u1: otherUid,
+                        u2: userID.value,
+                        senderUsername: currentUser.value,
+                        receiverUsername: otherUsername,
+                        pfp: Otherpfp,
+                      );
+                    },
+                  ),
+                );
+              });
+            },
+            borderRadius: BorderRadius.circular(20),
+            child: ListTile(
+              leading: Avatar(seed: Otherpfp, r: 25),
+              title: Text(otherUsername),
+              subtitle: Text(
+                data["lastMsg"] != null
+                    ? data["reciever"] == currentUser.value
+                          ? "${data["lastMsg"]}"
+                          : "You:${data["lastMsg"]}"
+                    : "",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: Text(
+                data["lastUpdated"] != null
+                    ? _formatTimestamp(data["lastUpdated"])
+                    : "",
+                style: const TextStyle(fontSize: 12),
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
-}
-
-
+    );
+  }
 
   String _formatTimestamp(Timestamp timestamp) {
-    return  DateFormat('hh:mm a').format(timestamp.toDate());
+    return DateFormat('hh:mm a').format(timestamp.toDate());
   }
 }
