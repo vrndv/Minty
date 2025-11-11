c3f785f75d7e3cf81468d5f27f03bbcfeed66357
vrndv <varundevnic@gmail.com>
2025-11-11 17:32:09 +0530

    update:Complete revamp on Auth Page

--- lib/screen/auth/authenticate.dart
+++ lib/screen/auth/authenticate.dart
@@ -1,7 +1,10 @@
+import 'package:SHADE/screen/views/widgets/auth_wave.dart';
+import 'package:SHADE/screen/views/widgets/login_btn.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:SHADE/screen/auth/login.dart';
 import 'package:SHADE/screen/auth/register.dart';
-import 'package:rive/rive.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:SHADE/dataNotifiers/notifier.dart';
 
 class Auth extends StatefulWidget {
@@ -12,160 +15,219 @@ class Auth extends StatefulWidget {
 }
 
 class _AuthState extends State<Auth> {
+  bool show = false;
+  bool flicker = false;
+  AnimatedTextController? MyAnim;
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 1500), () {
+      setState(() {
+        show = true;
+      });
+    });
+    flickerTimer();
+  }
+
+  flickerTimer() async {
+    while (true) {
+      try {
+        await Future.delayed(Duration(seconds: 10), () => playFlicker());
+      } catch (e) {}
+    }
+  }
+
+  playFlicker() {
+    setState(() {
+      show = false;
+      flicker = !flicker;
+    });
+    Future.delayed(Duration(milliseconds: 1500), () {
+      setState(() {
+        show = true;
+      });
+    });
+  }
+
+  final nxtpg = Login(
+    icon: Icons.login,
+    barTitle: "Login",
+    subText: "Don't have an account?",
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          ' SHADE',
-          style: TextStyle(
-            fontSize: 24,
-            color: currentTheme.value ? Colors.black : Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(40, 175, 173, 173),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Stack(
           children: [
-            const SizedBox(height: 20),
-            Icon(
-              Icons.all_inclusive,
-              size: 100,
-              color: currentTheme.value
-                  ? const Color.fromARGB(104, 0, 0, 0)
-                  : const Color.fromARGB(118, 255, 255, 255),
-            ),
-            Text(
-              'Welcome to SHADE',
-              style: TextStyle(
-                color: currentTheme.value ? Colors.black : Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 80),
-            ColorFiltered(
-              colorFilter: !currentTheme.value
-                  ? ColorFilter.matrix([
-                      -1,
-                      0,
-                      0,
-                      0,
-                      255,
-                      0,
-                      -1,
-                      0,
-                      0,
-                      255,
-                      0,
-                      0,
-                      -1,
-                      0,
-                      255,
-                      0,
-                      0,
-                      0,
-                      1,
-                      0,
-                    ])
-                  : ColorFilter.linearToSrgbGamma(),
-              child: const SizedBox(
-                height: 200,
-                width: 200,
-                child: RiveAnimation.asset(
-                  'assets/rive/pumping.riv',
-                  fit: BoxFit.contain,
-                  speedMultiplier: 1.1,
-                ),
-              ),
-            ),
-            Expanded(child: Text("")),
-            //LOGIN
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Login(
-                          icon: Icons.login,
-                          barTitle: "Login",
-                          subText: "Don't have an account?",
-                        );
-                      },
-                    ),
-                  );
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: currentTheme.value
-                        ? Colors.black
-                        : const Color.fromARGB(209, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: currentTheme.value ? Colors.white : Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
+            // LottieBuilder.asset(
+            //   'assets/rive/bg-sparkle.json',
+            //   height: double.infinity,
+            //   fit: BoxFit.fill,
+            // ),
+            AuthWave(), // Hero background
 
-            //REGISTER
             Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return Register(
-                          icon: Icons.app_registration,
-                          barTitle: "Register",
-                          subText: "Already a member?",
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 300),
+
+                  ValueListenableBuilder(
+                    valueListenable: currentTheme,
+                    builder: (context, value, child) {
+                      return Stack(
+                        children: [
+                          Container(
+                            width: 240,
+                            height: 80,
+                            //decoration: BoxDecoration(color: Colors.amber),
+                            child: Text(
+                              " Step into",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+
+                          show
+                              ? Positioned(
+                                  top: 15,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      playFlicker();
+                                    },
+                                    child: Text(
+                                      'SHADE',
+                                      style: GoogleFonts.orbitron(
+                                        textStyle: TextStyle(
+                                          color: value
+                                              ? Colors.black
+                                              : Colors.white,
+                                          fontSize: 55,
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              : Positioned(
+                                  top: 15,
+                                  child: AnimatedTextKit(
+                                    totalRepeatCount: 1,
+                                    controller: MyAnim,
+                                    key: ValueKey(flicker),
+                                    animatedTexts: [
+                                      FlickerAnimatedText(
+                                        'SHADE',
+                                        textStyle: GoogleFonts.orbitron(
+                                          textStyle: TextStyle(
+                                            color: value
+                                                ? Colors.black
+                                                : Colors.white,
+                                            fontSize: 55,
+                                            fontWeight: FontWeight.w900,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                        ],
+                      );
+                    },
+                  ),
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 60,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 7.0,
+                            color: Colors.white,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                      ),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          RotateAnimatedText('Where privacy meets connection.'),
+                          RotateAnimatedText('Talk, trust, together'),
+                          RotateAnimatedText('Big privacy, small phones.'),
+                          RotateAnimatedText(
+                            'Messages disappear. Trust remains.',
+                            textStyle: TextStyle(fontSize: 15),
+                          ),
+                          RotateAnimatedText('Every chat, a safe space.'),
+                          RotateAnimatedText('Even your old phone fits in.'),
+                          RotateAnimatedText("Every chat, a safe space."),
+                          RotateAnimatedText("Chat anywhere. No fuss."),
+                        ],
+
+                        repeatForever: true,
+                        pause: const Duration(milliseconds: 1000),
+                        displayFullTextOnTap: true,
+                        stopPauseOnTap: true,
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Text(""), flex: 1),
+                  //LOGIN
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Login(
+                                icon: Icons.login,
+                                barTitle: "Login",
+                                subText: "Don't have an account?",
+                              );
+                            },
+                          ),
                         );
                       },
+                      child: Hero(
+                        tag: "login_btn",
+                        child: Login_btn(text: "Login"),
+                      ),
                     ),
-                  );
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: double.infinity,
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: currentTheme.value
-                        ? Colors.black
-                        : const Color.fromARGB(199, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(5),
                   ),
-                  child: Text(
-                    "Register",
-                    style: TextStyle(
-                      color: currentTheme.value ? Colors.white : Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500,
+                  SizedBox(height: 10),
+                  //REGISTER
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Register(
+                                icon: Icons.app_registration,
+                                barTitle: "Register",
+                                subText: "Already a member?",
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Hero(
+                        tag: "reg_btn",
+                        child: Login_btn(text: "Register"),
+                      ),
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                ),
+                  Expanded(child: Text(""), flex: 1),
+                ],
               ),
             ),
-            const SizedBox(height: 40),
           ],
         ),
       ),
