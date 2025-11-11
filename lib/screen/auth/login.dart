d15ceb2d1eb36b0a3bbc22037f0bd8aded6f95d5
vrndv <varundevnic@gmail.com>
2025-11-11 17:45:10 +0530

    update : Complete revamp on Login Page

--- lib/screen/auth/login.dart
+++ lib/screen/auth/login.dart
@@ -1,3 +1,5 @@
+import 'package:SHADE/screen/views/widgets/auth_wave.dart';
+import 'package:SHADE/screen/views/widgets/login_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:SHADE/dataNotifiers/notifier.dart';
 import 'package:SHADE/database.dart';
@@ -70,10 +72,10 @@ class _LoginState extends State<Login> {
           context,
           MaterialPageRoute(
             builder: (context) {
-               currentPageNotifier.value = 0;
-                currentUser.value = uname;
-                userID.value = FirebaseAuth.instance.currentUser!.uid;
-              return DataBaseForm(userEmail: uname, page: 0,);
+              currentPageNotifier.value = 0;
+              currentUser.value = uname;
+              userID.value = FirebaseAuth.instance.currentUser!.uid;
+              return DataBaseForm(userEmail: uname, page: 0);
             },
           ),
           (route) => false,
@@ -95,198 +97,214 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.barTitle)),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 20),
-                Icon(widget.icon, size: 50, color:currentTheme.value ? const Color.fromARGB(104, 0, 0, 0) : const Color.fromARGB(118, 255, 255, 255)),
-                SizedBox(height: 20),
-                Text(
-                  "Welcome back! we missed you!",
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(height: 80),
-                Text(
-                  err ?? "",
-                  style: TextStyle(color: Colors.red, fontSize: 16),
-                ),
-                SizedBox(height: 20),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            AuthWave(),
 
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      labelStyle: TextStyle(color:Colors.grey ),
-                      hint: Text(
-                        "myself@abc.com ",
-                        style: TextStyle(
-                          color: currentTheme.value
-                              ? Colors.black26
-                              : Colors.white30,
-                        ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: currentTheme.value
-                              ? Colors.black
-                              : const Color.fromARGB(82, 255, 254, 254),
-                          width: 1.5,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: currentTheme.value? Colors.white: const Color.fromARGB(255, 167, 129, 86),
-                              
-                          width: 1.5,
-                        ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 20),
-
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: TextField(
-                    controller: pwController,
-
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      labelStyle: TextStyle(color:Colors.grey ),
-                      hint: Text(
-                        "%abc@123# ",
-                        style: TextStyle(
-                          color: currentTheme.value
-                              ? Colors.black26
-                              : Colors.white30,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: currentTheme.value
-                              ? Colors.black
-                              : Color.fromARGB(255, 167, 129, 86),
-                          width: 1.5,
-                        ),
-                      ),
-                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: currentTheme.value
-                              ? Colors.black
-                              : const Color.fromARGB(82, 255, 254, 254),
-                          width: 1.5,
-                        ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsetsGeometry.only(right: 20, top: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+            SingleChildScrollView(
+              child: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      GestureDetector(
-                        onTap: () async {
-                          if (emailController.text.contains("@")) {
-                            await FirebaseAuth.instance.sendPasswordResetEmail(
-                              email: emailController.text,
-                            );
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(
-                                  "Reset link sent to: ${emailController.text}\nChech Spam folders ",
-                                ),
-                                duration: Duration(seconds: 5),
+                      SizedBox(height: 200),
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Welcome back! \n',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 28,
                               ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("Enter a valid Email ID"),
-                                duration: Duration(seconds: 2),
+                            ),
+                            TextSpan(
+                              text: ' we missed you!', 
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 20,
                               ),
-                            );
-                          }
-                        },
-                        child: Text(
-                          "Forgot Password?",
-                          style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          ],
                         ),
                       ),
+
+                      SizedBox(height: 30),
+                      Text(
+                        err ?? "",
+                        style: TextStyle(color: Colors.red, fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            labelStyle: TextStyle(color: Colors.grey),
+                            
+                            hint: Text(
+                              "myself@abc.com ",
+                              style: TextStyle(
+                                color: currentTheme.value
+                                    ? Colors.black26
+                                    : Colors.white30,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: currentTheme.value
+                                    ? Colors.black
+                                    : const Color.fromARGB(82, 255, 254, 254),
+                                width: 1.5,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: currentTheme.value
+                                    ? Colors.white
+                                    : const Color.fromARGB(255, 167, 129, 86),
+
+                                width: 1.5,
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: TextField(
+                          controller: pwController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            
+                            labelText: "Password",
+                            labelStyle: TextStyle(color: Colors.grey),
+                            hint: Text(
+                              "%abc@123# ",
+                              style: TextStyle(
+                                color: currentTheme.value
+                                    ? Colors.black26
+                                    : Colors.white30,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: currentTheme.value
+                                    ? Colors.black
+                                    : Color.fromARGB(255, 167, 129, 86),
+                                width: 1.5,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: currentTheme.value
+                                    ? Colors.black
+                                    : const Color.fromARGB(82, 255, 254, 254),
+                                width: 1.5,
+                              ),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsetsGeometry.only(right: 20, top: 5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            GestureDetector(
+                              onTap: () async {
+                                if (emailController.text.contains("@")) {
+                                  await FirebaseAuth.instance
+                                      .sendPasswordResetEmail(
+                                        email: emailController.text,
+                                      );
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                        "Reset link sent to: ${emailController.text}\nChech Spam folders ",
+                                      ),
+                                      duration: Duration(seconds: 5),
+                                    ),
+                                  );
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text("Enter a valid Email ID"),
+                                      duration: Duration(seconds: 2),
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Text(
+                                "Forgot Password?",
+                                style: TextStyle(fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 40),
+
+                      Center(
+                        child: GestureDetector(
+                          onTap: () {
+                            login();
+                          },
+                          child: Hero(
+                            tag: "login_btn",
+                            child: Login_btn(text: "Login"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(widget.subText),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return Register(
+                                      icon: Icons.app_registration,
+                                      barTitle: "Register",
+                                      subText: "Already a member?",
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                            child: Text(
+                              (widget.barTitle == "Login")
+                                  ? "  Register"
+                                  : "  Login",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 70),
                     ],
                   ),
                 ),
-                SizedBox(height: 40),
-
-                Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      // Handle login logic here
-                      login();
-                    },
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      margin: EdgeInsets.symmetric(horizontal: 20),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                      child: Center(
-                        child: Text(
-                          widget.barTitle,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(widget.subText),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return Register(
-                                icon: Icons.app_registration,
-                                barTitle: "Register",
-                                subText: "Already a member?",
-                              );
-                            },
-                          ),
-                        );
-                      },
-                      child: Text(
-                        (widget.barTitle == "Login") ? "  Register" : "  Login",
-                        style: TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 70),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
